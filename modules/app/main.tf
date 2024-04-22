f4af4262b2979345eb97c022401c39625e3fc6d4
Pavani <vemasani.pavani@gmail.com>
2024-04-22 10:27:45 +0530

    attached aws instance profile to ec2 instance

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,16 +1,17 @@
-//resource "aws_instance" "prometheus" {
-//  ami = var.ami
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.security.id]
-//  tags = {
-//    Name = var.tools_name
-//  }
-//}
-//data "aws_security_group" "security" {
-//  name = "allow-all"
-//}
+resource "aws_instance" "prometheus" {
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.security.id]
+  tags = {
+    Name = var.tools_name
+  }
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+}
+data "aws_security_group" "security" {
+  name = "allow-all"
+}
 
-//create Prometheus role
+//create Prometheus roles
 resource "aws_iam_role" "role" {
   name = "Prometheus-role"
   assume_role_policy = jsonencode({
@@ -32,7 +33,7 @@ resource "aws_iam_role" "role" {
   }
 }
 //it is a container
-resource "aws_iam_instance_profile" "test_profile" {
+resource "aws_iam_instance_profile" "instance_profile" {
   name = "Prometheus-role"
   role = aws_iam_role.role.name
 }
